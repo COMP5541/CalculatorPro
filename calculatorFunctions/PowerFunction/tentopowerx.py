a7b031b203cfc18bfe140127af4ce36acbdb9f68
faeze-mobasheri <35981490+faeze-mobasheri@users.noreply.github.com>
2018-02-21 14:44:02 -0500

    Implementation of Power function and its testing

---
+++ calculatorFunctions/PowerFunction/tentopowerx.py
@@ -0,0 +1,71 @@
+__author__ = "The CalculatorPro Inc."
+__copyright__ = "Copyright 2018, CalculatorPro"
+__credits__ = ["Team A"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Faezeh Mobasheri"
+__email__ = "f_mobas@encs.concordia.ca"
+__status__ = "Release v1.0"
+
+import math
+
+value = 1
+ln10 = math.log(10)
+
+#Define precision decimal
+error=0.000000000001
+error_decimals=10
+
+
+#Calculation of power function
+def power(base,exp):
+    if (exp==0):
+        return 1
+    if(exp==1):
+        return(base)
+    if(exp!=1):
+        return(base*power(base,exp-1))
+
+
+#calculation of factorial function
+def factorial(n):
+        if n == 1:
+            return n
+        if n==0:
+            return 1
+        else:
+            return n * factorial(n - 1)
+
+
+#if the value of X is an integer use power function, if it is floating number taylor series formula
+def tentopower(x):
+    val=x
+    if type(val)==int:
+            if val >= 0 :
+               print(power(10, val))
+            if val < 0 :
+               val *= -1
+            print(1/power(10, val))
+    elif type(val)==float:
+        if val>=0:
+            value = 0.0000000000000000000001
+            for n in range(150):
+                calculate = ((power(ln10, n)) * (power(val + 22, n)) * 0.0000000000000000000001) / factorial(n)
+                if calculate > error:
+                  value += calculate
+                else:
+                  value += 0
+                  round(calculate, error_decimals)
+            return print(value)
+        if val<0:
+            val *= -1
+            value = 0.0000000000000000000001
+            for n in range(150):
+                calculate = ((power(ln10, n)) * (power(val + 22, n)) * 0.0000000000000000000001) / factorial(n)
+                if calculate > error:
+                  value += calculate
+                else:
+                  value += 0
+                  round(calculate, error_decimals)
+            return print(1/value)
+
