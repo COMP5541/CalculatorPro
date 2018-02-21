55e3e6f02270f186403e7c7c65104255a05425e1
faeze-mobasheri <35981490+faeze-mobasheri@users.noreply.github.com>
2018-02-21 15:29:48 -0500

    Create test.py

---
+++ calculatorFunctions/PowerFunction/test.py
@@ -0,0 +1,49 @@
+__author__ = "The CalculatorPro Inc."
+__copyright__ = "Copyright 2018, CalculatorPro"
+__credits__ = ["Team A"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Faezeh Mobasheri"
+__email__ = "f_mobas@encs.concordia.ca"
+__status__ = "Release v1.0"
+
+
+from calculatorFunctions.PowerFunction.tentopowerx import tentopower
+import math
+
+#Calculation Test1
+test1 = 2.2
+print("test case 1:")
+print("If x is floating number and positive calculated result is:")
+print(tentopower(test1))
+print("If x is floating number and positive returned result form pyhton is:")
+print(10**test1)
+
+#Calculation Test2
+print("\n")
+print("test case 2")
+test2= -4.2
+print("If x is floating number and negative calculated result is:")
+print(tentopower(test2))
+print("If x is floating number and negative returned result form pyhton is:")
+print(10**test2)
+
+#power function test
+print("\n")
+print("test case 3")
+print("If x is an integer and positive number calculated result is:")
+print(tentopower(6))
+print("If x is an integer and positive number returned result from Python is:")
+print(math.pow(10, 6))
+
+
+#factorial function test
+print("\n")
+print("test case 4")
+print("If x is an integer and negative number calculated result is:")
+print(tentopower(-3))
+print("If x is an integer and positive number returned result from Python is:")
+print(math.pow(10, -3))
+
+
+
