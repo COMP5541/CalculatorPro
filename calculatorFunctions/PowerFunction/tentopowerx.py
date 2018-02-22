453e084a1ce5cb84eacd45436994412a7f835fc0
faeze-mobasheri <35981490+faeze-mobasheri@users.noreply.github.com>
2018-02-22 09:21:33 -0500

    Merge branch 'master' of https://github.com/COMP5541/CalculatorPro
    # Conflicts:
    #	calculatorFunctions/PowerFunction/tentopowerx.py
    #	calculatorFunctions/PowerFunction/test.py
    #	calculatorFunctions/TenToPowerX/tentopowerx.py

--- calculatorFunctions/PowerFunction/tentopowerx.py
+++ calculatorFunctions/PowerFunction/tentopowerx.py
@@ -7,75 +7,15 @@ __maintainer__ = "Faezeh Mobasheri"
 __email__ = "f_mobas@encs.concordia.ca"
 __status__ = "Release v1.0"
 
-from calculatorFunctions.commonFunctions.commonFunctions import power
-from calculatorFunctions.commonFunctions.commonFunctions import factorial
-from calculatorFunctions.log.ln import loge
+import math
+#from calculatorFunctions.log.ln import loge
+#from calculatorFunctions.Exponential.funEx import fun_ex
 
-
-#import math
-
-value = 1
-ln10 = loge(10)
-
-#Define precision decimal
-error=0.000000000001
-error_decimal=10
-
-
-#Calculation of power function
-#def power(base,exp):
-#    if (exp==0):
-#        return 1
-#    if(exp==1):
-#        return(base)
-#    if(exp!=1):
-#       return(base*power(base,exp-1))
-
-
-#calculation of factorial function
-#def factorial(n):
-#        if n == 1:
-#            return n
-#        if n==0:
-#            return 1
-#        else:
-#            return n * factorial(n - 1)
-
-
-#if the value of X is an integer use power function, if it is floating number taylor series formula
 def tentopower(x):
-    val=x
-    if type(val)==int:
-            if val >= 0 :
-               return power(10, val)
-            if val < 0 :
-               val *= -1
-            return 1/power(10, val)
-    elif type(val)==float:
-        if val>=0:
-            value = 0.0000000000000000000001
-            for n in range(150):
-                calculate = ((power(ln10, n)) * (power(val + 22, n)) * 0.0000000000000000000001) / factorial(n)
-                if calculate > error:
-                  value += calculate
-                else:
-                  value += 0
-                  round(calculate, error_decimal)
-            return value
-        if val<0:
-            val *= -1
-            value = 0.0000000000000000000001
-            for n in range(150):
-                calculate = ((power(ln10, n)) * (power(val + 22, n)) * 0.0000000000000000000001) / factorial(n)
-                if calculate > error:
-                  value += calculate
-                else:
-                  value += 0
-                  round(calculate, error_decimal)
-            return 1/value
+    A=math.log(10)
+    B=x*A
+    result=math.exp(B)
+    return result
 
-
-#testing accuracy
-#print(tentopower(2.2))
-#print(factorial(5))
-#print(power(10, 3))
+print(tentopower(100.8))
+print(10**100.8)
