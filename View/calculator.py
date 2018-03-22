3565dec8957cb106ec6e5be9c4d97c9f7e517fe6
faeze-mobasheri <35981490+faeze-mobasheri@users.noreply.github.com>
2018-03-22 13:15:45 -0400

    ETERNITY GUI

--- View/calculator.py
+++ View/calculator.py
@@ -9,6 +9,7 @@ __status__ = "Release v1.0"
 
 
 from tkinter import *
+import parser
 
 from Model import funEx, ln, sin, squareroot, tentopowerx
 
@@ -26,6 +27,20 @@ class calculator:
 
     def inputVal(self, argi):
         self.e.insert(END, argi)
+
+    def inputOperator(self, operator):
+        self.e.insert(END, operator)
+
+    def calculate(self):
+        self.value= eval(self.e.get())
+        try:
+            formula = self.e.get()#parser.expr(self.value).compile()
+            result = eval(formula)
+            self.clearAll()
+            self.e.insert(0, result)
+        except Exception:
+            self.clearAll()
+            self.e.insert(0, "Error!")
 
 
     # Functions
@@ -69,14 +84,13 @@ class calculator:
 
 
 
-
     # Initialize calculator
 
     def __init__(self, master):
-        master.title('CalulatorPro')
+        master.title('ETERNITY')
         master.geometry()
-        self.e = Entry(master)
-        self.e.grid(row=0, column=0, columnspan=6 )
+        self.e = Entry(master, font = ("Times", 15, 'bold'))
+        self.e.grid(row=0, column=0, columnspan=10, sticky = W+E )
         self.e.focus_set()
 
 
@@ -85,29 +99,39 @@ class calculator:
         # Creating Buttons
 
         # Number Pad
-        Button(master, text="7", width=3, command=lambda: self.inputVal(7)).grid(row=1, column=0)
-        Button(master, text="4", width=3, command=lambda: self.inputVal(4)).grid(row=2, column=0)
-        Button(master, text="1", width=3, command=lambda: self.inputVal(1)).grid(row=3, column=0)
+        Button(master, text="7", width=3, command=lambda: self.inputVal(7), font=("times", 15)).grid(row=1, column=0)
+        Button(master, text="4", width=3, command=lambda: self.inputVal(4), font=("times", 15)).grid(row=2, column=0)
+        Button(master, text="1", width=3, command=lambda: self.inputVal(1), font=("times", 15)).grid(row=3, column=0)
+        Button(master, text="0", width=3, command=lambda: self.inputVal(0), font=("times", 15)).grid(row=4, column=0)
 
-        Button(master, text="8", width=3, command=lambda: self.inputVal(8)).grid(row=1, column=1)
-        Button(master, text="5", width=3, command=lambda: self.inputVal(5)).grid(row=2, column=1)
-        Button(master, text="2", width=3, command=lambda: self.inputVal(2)).grid(row=3, column=1)
-        Button(master, text="0", width=3, command=lambda: self.inputVal(0)).grid(row=4, column=1)
+        Button(master, text="8", width=3, command=lambda: self.inputVal(8), font=("times", 15)).grid(row=1, column=1)
+        Button(master, text="5", width=3, command=lambda: self.inputVal(5), font=("times", 15)).grid(row=2, column=1)
+        Button(master, text="2", width=3, command=lambda: self.inputVal(2), font=("times", 15)).grid(row=3, column=1)
+        Button(master, text=".", width=3, command=lambda: self.inputOperator('.'), font=("times", 15)).grid(row=4, column=1)
 
-        Button(master, text="9", width=3, command=lambda: self.inputVal(9)).grid(row=1, column=2)
-        Button(master, text="6", width=3, command=lambda: self.inputVal(6)).grid(row=2, column=2)
-        Button(master, text="3", width=3, command=lambda: self.inputVal(3)).grid(row=3, column=2)
+        Button(master, text="9", width=3, command=lambda: self.inputVal(9), font=("times", 15)).grid(row=1, column=2)
+        Button(master, text="6", width=3, command=lambda: self.inputVal(6), font=("times", 15)).grid(row=2, column=2)
+        Button(master, text="3", width=3, command=lambda: self.inputVal(3), font=("times", 15)).grid(row=3, column=2)
+        Button(master, text="=", width=3, command=lambda: self.calculate('='), font=("times", 15), foreground = "red").grid(row=4, column=2)
 
 
         # Functions
-        Button(master, text='AC', width=3, command=lambda: self.clearAll()).grid(row=1, column=4)
-        Button(master, text='C', width=3, command=lambda: self.deleteOne()).grid(row=2, column=4)
-        Button(master, text="√", width=3, command=lambda: self.squareroot()).grid(row=3, column=4)
-        Button(master, text="ln(x)", width=3, command=lambda: self.ln()).grid(row=4, column=4)
+        Button(master, text='C', width=3, command=lambda: self.deleteOne(), font=("times", 15), foreground="red").grid(row=1, column=4)
+        Button(master, text="*", width=3, command=lambda: self.inputOperator('*'), font=("times", 15)).grid(row=2, column=4)
+        Button(master, text="+", width=3, command=lambda: self.inputOperator('+'), font=("times", 15)).grid(row=3,column=4)
+        Button(master, text="(", width=3, command=lambda: self.inputOperator('('), font=("times", 15)).grid(row=4,column=4)
 
-        Button(master, text="sin", width=3, command=lambda: self.sine()).grid(row=1, column=5)
-        Button(master, text="e^(x)", width=3, command=lambda: self.exFunc()).grid(row=2, column=5)
-        Button(master, text="10^x", width=3, command=lambda: self.tenToPower()).grid(row=3, column=5)
+        Button(master, text='AC', width=3, command=lambda: self.clearAll(), font=("times", 15),foreground="red").grid(row=1, column=5)
+        Button(master, text="/", width=3, command=lambda: self.inputOperator('/'), font=("times", 15)).grid(row=2,column=5)
+        Button(master, text="-", width=3, command=lambda: self.inputOperator('-'), font=("times", 15)).grid(row=3,column=5)
+        Button(master, text=")", width=3, command=lambda: self.inputOperator(')'), font=("times", 15)).grid(row=4,column=5)
+
+        Button(master, text="√", width=3, command=lambda: self.squareroot(), font=("times", 15)).grid(row=1, column=6)
+        Button(master, text="ln(x)", width=3, command=lambda: self.ln(), font=("times", 15)).grid(row=2, column=6)
+        Button(master, text="sin", width=3, command=lambda: self.sine(), font=("times", 15)).grid(row=3, column=6)
+        Button(master, text="e^(x)", width=3, command=lambda: self.exFunc(), font=("times", 15)).grid(row=4, column=6)
+
+        Button(master, text="10^x", width=3, command=lambda: self.tenToPower(), font=("times", 15)).grid(row=1, column=7)
 
 
 
