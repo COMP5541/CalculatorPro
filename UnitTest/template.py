16871e4324e137ede481378e77e5d10fc28f9f18
Gus <a_gimene@encs.concordia.ca>
2018-03-25 13:37:58 -0400

    Testing template.

--- UnitTest/template.py
+++ UnitTest/template.py
@@ -5,3 +5,19 @@ from Model.sin import *
 from Model.squareroot import *
 from Model.tentopowerx import *
 
+#Define test functions
+def test(x):
+    print("ETERNITY Test for " + str(x))
+    print(str(ln(x)))
+
+def run_test():
+
+    test(test_max)
+    test(test_min)
+    test(test_small)
+    test(0)
+    test(1)
+
+# TEST
+if __name__ == "__main__":
+    run_test()
--- UnitTest/test_ln.py
+++ UnitTest/test_ln.py
@@ -2,20 +2,19 @@ from Model.config import *
 from Model.log import *
 import math
 
-def test_ln():
-    max = 10 ** 10
-    min = -max
-    small = 10 ** (-10)
+#Define test functions
+def test(x):
+    print("ETERNITY Test for " + str(x))
+    print(str(ln(x)))
 
-    print("Test ln(x):")
-    print(ln(max))
-    print(ln(min))
-    print(ln(small))
+def run_test():
 
-    print(math.log(max))
-    #print(math.log(min))
-    print(math.log(small))
+    test(test_max)
+    test(test_min)
+    test(test_small)
+    test(0)
+    test(1)
 
 #TEST
 if __name__ == "__main__":
-    test_ln()
+    run_test()
